56771a9a06a7676fdfdc1279a6e4c240dcb11725
PabloAlonsoRomero <pabloealonsor@gmail.com>
2026-06-04 00:50:20 -0600

    feat: implementacion del endpoint de registro y logica

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import authRouter from './modules/auth/auth.router';
+import { errorHandler } from './core/middlewares/error.middleware';
 
 dotenv.config();
 
@@ -13,6 +15,12 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.json({ message: 'BeatFlow API running successfully!' });
 });
+
+// Routes
+app.use('/api/auth', authRouter);
+
+// Global Error Handler
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
